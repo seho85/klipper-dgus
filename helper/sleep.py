e0137046d18c2ad3ce72b25f02172321916abc23
Zeanon <thezeanon@gmail.com>
2023-07-14 15:43:57 +0200

    Update and rename sleep.py to sleep.py

--- helper/sleep.py
+++ helper/sleep.py
@@ -2,4 +2,9 @@ import time, sys
 
 sleep_time = sys.argv[1] if len(sys.argv) > 1 else 15
 
+try:
+        sleep_time = int(sleep_time)
+except ValueError:
+        sleep_time = 15
+
 time.sleep(sleep_time)
